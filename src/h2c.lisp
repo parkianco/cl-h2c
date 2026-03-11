5bd6273483919496b88df751a63d36aa314857e4
Park Ian Co <opensource@parkian.co>
2026-03-11 19:49:55 -0400

    Fix copyright attribution and add headers

--- src/h2c.lisp
+++ src/h2c.lisp
@@ -1,3 +1,6 @@
+;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
 ;;;; h2c.lisp
 ;;;; Hash-to-curve for secp256k1 per IETF draft-irtf-cfrg-hash-to-curve
 
